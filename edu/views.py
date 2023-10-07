156434efa274d70f821fb4bb5ce3b6a61d116830
dohyun17 <143243417+dohyun17@users.noreply.github.com>
2023-10-07 14:51:05 +0900

    DB에 저장하기

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
+from.models import Feed
 # Create your views here.
 class Index(View):
     template_name = 'index.html'
@@ -12,4 +13,20 @@ class TagStudy(View):
     template_name = 'tag_study.html'
 
     def get(self, request):
+        
+        feeds = Feed.objects.all().order_by("id")
+        return render(request, self.template_name,{'feed_list' : feeds})
+    
+class NewContent(View):
+    template_name = 'upload_form.html'
+
+    def get(self, request):
         return render(request, self.template_name)
+    
+    def post(self, request):
+        param = request.POST.get("content")
+        print("전달받은 내용:" + param)
+        feed = Feed(content=param)
+        feed.save()
+        return redirect('edu:tag_study')
+           
